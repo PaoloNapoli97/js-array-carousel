5480f3f2f0589ec26937635a67924586a76df5d5
PaoloNapoli97 <paolonapoli@live.it>
2022-10-26 17:38:49 +0200

    I give up.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 "use strict"
 
-const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg']
-let items = document.getElementsByClassName("items");
+const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
+const target = document.querySelector('.container .items');
 
 for (let i = 0; i < images.length; i++) {
-    images.push(`<div class="item"><img src="${images[i]}></div>`);
+    var item = document.createElement('div') = `<div class="item"><img src="${images[i]}"></div>`;
+    document.body.appendChild(item);
 }
