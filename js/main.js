67a1e985ecdead9c1a761b69ac37f0b8d3b68b26
PaoloNapoli97 <paolonapoli@live.it>
2022-10-26 22:55:08 +0200

    Add array img and events

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 "use strict"
 
 const images = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
-const target = document.querySelector('.container .items');
+const items = document.querySelector('.container .items');
+let active = 0;
+
 
 for (let i = 0; i < images.length; i++) {
-    var item = document.createElement('div') = `<div class="item"><img src="${images[i]}"></div>`;
-    document.body.appendChild(item);
+    items.innerHTML += (`<div class="item"><img src="${images[i]}"></div>`);
 }
+// document.querySelector('.item').classList.remove('item');
+document.querySelector('.item').classList.add('item-active');
+// document.querySelector('.item').classList.remove('item');
+let img = document.querySelectorAll('.item');
+
+document.querySelector('.next').addEventListener("click" , function(){
+
+    img[active].classList.remove('item-active');
+    active += 1;
+    img[active].classList.add('item-active');
+    if(active == img.length - 1){
+        img[active].classList.remove('item-active');
+        active = 0;
+        img[active].classList.add('item-active');
+    }
+})
+
+document.querySelector('.prev').addEventListener("click" , function(){
+    img[active].classList.remove('item-active');
+    active -= 1;
+    img[active].classList.add('item-active');
+})
